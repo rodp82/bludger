3fda9215960374ebd8d4e174d2c137ffda4c4e9f
Rod Pattison <rodpattison@gmail.com>
2022-05-09 20:34:36 +0800

    Added user, transactions, categories nest modules

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,9 +3,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountsModule } from './accounts/accounts.module';
+import { UsersModule } from './users/users.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [AccountsModule],
+  imports: [AccountsModule, UsersModule, TransactionsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
